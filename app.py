1055f796566023508f260152004c76417b63999f
pdTrade <pd.pub.dev@gmail.com>
2024-03-10 01:55:41 +0900

    ログイン認証のデコレータ

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-
 from models import Entry
 
+def login_required(view):
+  @wraps(view)
+  def wrap(*args, **kwargs):
+      if not session.get('logged_in'):
+        return redirect(url_for('login'))
+      return view(*args, **kwargs)
+  return wrap
+
 @app.route('/')
+@login_required
 def show_entries():
-  if not session.get('logged_in'):
-    return redirect('/login')
   entries = Entry.query.order_by(Entry.id.desc()).all()
   return render_template('index.html', entries=entries)
 
@@ -39,16 +46,13 @@ if __name__ == '__main__':
   app.run(debug=True)
 
 @app.route('/entries/new')
+@login_required
 def new_entry():
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
   return render_template('new.html')
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
-
   entry = Entry(
     title=request.form['title'],
     text=request.form['text']
@@ -62,23 +66,20 @@ def add_entry():
   return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>')
+@login_required
 def show_entry(id):
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
   entry = Entry.query.get(id)
   return render_template('show.html', entry=entry)
 
 @app.route('/entries/<int:id>/edit')
+@login_required
 def edit_entry(id):
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
   entry = Entry.query.get(id)
   return render_template('edit.html', entry=entry)
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
 def update_entry(id):
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
   entry = Entry.query.get(id)
   entry.title = request.form['title']
   entry.text = request.form['text']
@@ -91,9 +92,8 @@ def update_entry(id):
   return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
 def delete_entry(id):
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
   entry = Entry.query.get(id)
 
   db.session.delete(entry)
